593db73e154feac37a303efc60913490f051d794
u0939404 <u0939404@kcmac-05.scl.utah.edu>
2017-01-17 15:26:10 -0700

    Everything looks good, centered only using absolute coordinates

--- Homework 1/Homework 1/AppDelegate.swift
+++ Homework 1/Homework 1/AppDelegate.swift
@@ -12,25 +12,91 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    
+    var _X: UILabel?
+    var _Y: UILabel?
+    var _XValue: UILabel?
+    var _YValue: UILabel?
+    
+    var _XPlusY: UILabel?
+    var _XMultY: UILabel?
+    
+    var _XSlider: UISlider?
+    var _YSlider: UISlider?
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         
         window = UIWindow()
         window?.rootViewController = ViewController()
-        window?.rootViewController?.view.backgroundColor = UIColor.greenColor()
+        window?.rootViewController?.view.backgroundColor = UIColor.lightGrayColor()
         window?.makeKeyAndVisible()
         
-        let lightSwitch: UISwitch = UISwitch()
-        lightSwitch.frame = CGRect(x: 50.0, y: 30.0, width: 100.0, height: 40.0)
-        window?.rootViewController?.view.addSubview(lightSwitch)
+        // Set up sliders
+        _XSlider = UISlider(frame: CGRectMake(60, 175, 175, 20))
+        _XSlider!.minimumValue = 0.0
+        _XSlider!.maximumValue = 1.0
+        _XSlider!.addTarget(self, action: #selector(AppDelegate.sliderChanged)
+            , forControlEvents: UIControlEvents.ValueChanged)
+        window!.addSubview(_XSlider!)
         
-        //Hopefully this works good
-        //Testing remote connection
+        _YSlider = UISlider(frame: CGRectMake(60, 225, 175, 20))
+        _YSlider!.minimumValue = 0.0
+        _YSlider!.maximumValue = 1.0
+        _YSlider!.addTarget(self, action: #selector(AppDelegate.sliderChanged), forControlEvents: UIControlEvents.ValueChanged)
+        window!.addSubview(_YSlider!)
+        
+        // Set up all labels
+        _X = UILabel(frame: CGRectMake(40, 175, 10, 20))
+        _X!.text = "X"
+        window!.addSubview(_X!)
+        
+        _XValue = UILabel(frame: CGRectMake(250, 175, 100, 20))
+        _XValue!.text = String(format: "%.3f", _XSlider!.value)
+        window!.addSubview(_XValue!)
+        
+        _Y = UILabel(frame: CGRectMake(40, 225, 10, 20))
+        _Y!.text = "Y"
+        window!.addSubview(_Y!)
+        
+        _YValue = UILabel(frame: CGRectMake(250, 225, 100, 20))
+        _YValue!.text = String(format: "%.3f", _YSlider!.value)
+        window!.addSubview(_YValue!)
+        
+        _XPlusY = UILabel(frame: CGRectMake(110, 275, 150, 20))
+        _XPlusY!.text = "X + Y: " + String(format: "%.3f", _XSlider!.value)
+        window!.addSubview(_XPlusY!)
+        
+        _XMultY = UILabel(frame: CGRectMake(110, 300, 150, 20))
+        _XMultY!.text = "X * Y: " + String(format: "%.3f", _YSlider!.value)
+        window!.addSubview(_XMultY!)
+        
         
         return true
     }
 
+    
+    func sliderChanged()
+    {
+        let x = _XSlider!.value
+        let y = _YSlider!.value
+        
+        
+        // Update slider values
+        _XValue!.text = String(format: "%.3f", x)
+        _YValue!.text = String(format: "%.3f", y)
+        
+        // Do calculations
+        let xPlusy = x + y
+        let xMulty = x * y
+        
+        
+        // Update calculations
+        _XPlusY!.text = "X + Y: " + String(format: "%.3f", xPlusy)
+        _XMultY!.text = "X * Y: " + String(format: "%.3f", xMulty)
+    }
+    
+    
     func applicationWillResignActive(application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
