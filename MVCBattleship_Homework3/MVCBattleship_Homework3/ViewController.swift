e29297a232ed8e763a6dccd0d87398ca536682d4
u0939404 <u0939404@kcmac-48.scl.utah.edu>
2017-03-10 14:18:33 -0700

    created Battleship project

---
+++ MVCBattleship_Homework3/MVCBattleship_Homework3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MVCBattleship_Homework3
+//
+//  Created by u0939404 on 3/10/17.
+//  Copyright © 2017 mckay fenn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
