72fcd778ee26a2dca666827373c08bb7beb8fb5c
u0939404 <u0939404@kcmac-43.scl.utah.edu>
2017-03-20 11:35:51 -0600

    Kind of randomly placing ships

--- MVCBattleship_Homework3/MVCBattleship_Homework3/GameModel.swift
+++ MVCBattleship_Homework3/MVCBattleship_Homework3/GameModel.swift
@@ -24,8 +24,166 @@ class Game {
     private var currentPlayer1: Bool = false // start out with p1
     
     
+    
+    init() {
+        placeShips()
+        
+    }
+    
+    ///
+    /// Place ships randomly
+    ///
+    private func placeShips() {
+        // List of Ships
+        let ships = [5, 4, 3, 3, 2]
+        
+        
+        for ship in ships {
+            
+            let randX = Int(arc4random_uniform(10)) // column
+            let randY = Int(arc4random_uniform(10)) // row
+            let orientation = Int(arc4random_uniform(2))
+            
+            var validPlace = true
+            
+            if (orientation == 0)  { // Horizontal
+                var xPos = randX
+                for length: Int in 0 ..< ship {
+                    
+                    if (xPos < 9 && _p1Grid[randY][xPos] == .none) {
+                        validPlace = true
+                    }
+                    else {
+                        validPlace = false
+                    }
+                    
+                    // If all positions were valid then place the ship
+                    if (length == ship - 1 && validPlace) {
+                        setShip(grid: 1, ship: ship, randY: randY, randX: randX, orientation: orientation)
+                    }
+                    
+                    xPos = xPos + 1
+                }
+            }
+            else { // Vertical
+                var yPos = randY
+                for length: Int in 0 ..< ship {
+                    
+                    if (yPos < 9 && _p1Grid[yPos][randX] == .none) {
+                        validPlace = true
+                    }
+                    else {
+                        validPlace = false
+                    }
+                    
+                    // If all positions were valid then place the ship
+                    if (length == ship - 1 && validPlace) {
+                        setShip(grid: 1, ship: ship, randY: randY, randX: randX, orientation: orientation)
+                    }
+                    
+                    yPos = yPos + 1
+                }
+            }
+        }
+        
+        
+        
+        
+        // Now do it again for grid2
+        let ships2 = [5, 4, 3, 3, 2]
+        
+        
+        for ship in ships2 {
+            
+            let randX = Int(arc4random_uniform(10)) // column
+            let randY = Int(arc4random_uniform(10)) // row
+            let orientation = Int(arc4random_uniform(2))
+            
+            var validPlace = true
+            
+            if (orientation == 0)  { // Horizontal
+                var xPos = randX
+                for length: Int in 0 ..< ship {
+                    
+                    if (xPos < 9 && _p1Grid[randY][xPos] == .none) {
+                        validPlace = true
+                    }
+                    else {
+                        validPlace = false
+                    }
+                    
+                    // If all positions were valid then place the ship
+                    if (length == ship - 1 && validPlace) {
+                        setShip(grid: 2, ship: ship, randY: randY, randX: randX, orientation: orientation)
+                    }
+                    
+                    xPos = xPos + 1
+                }
+            }
+            else { // Vertical
+                var yPos = randY
+                for length: Int in 0 ..< ship {
+                    
+                    if (yPos < 9 && _p1Grid[yPos][randX] == .none) {
+                        validPlace = true
+                    }
+                    else {
+                        validPlace = false
+                    }
+                    
+                    // If all positions were valid then place the ship
+                    if (length == ship - 1 && validPlace) {
+                        setShip(grid: 2, ship: ship, randY: randY, randX: randX, orientation: orientation)
+                    }
+                    
+                    yPos = yPos + 1
+                }
+            }
+        }
+    }
+    
+    private func setShip(grid: Int, ship:Int, randY: Int, randX: Int, orientation: Int) {
+        var xPos = 0
+        var yPos = 0
+        
+        if (orientation == 0) {
+            xPos = randX
+        }
+        else {
+            yPos = randY
+        }
+
+        
+        for length: Int in 0 ..< ship {
+            if (orientation == 0) {
+                if (grid == 1) {
+                    _p1Grid[randY][xPos] = .ship
+                }
+                else {
+                    _p2Grid[randY][xPos] = .ship
+                }
+                
+                xPos = xPos + 1
+            }
+            else {
+                
+                if (grid == 1) {
+                    _p1Grid[yPos][randX] = .ship
+                }
+                else {
+                    _p2Grid[yPos][randX] = .ship
+                }
+                
+                yPos = yPos + 1
+            }
+        }
+    }
+    
+    
+    
+    
     // ENEMY ships
-    private var _p1Grid: [[Ships]] = [[.ship, .ship, .none, .none, .none, .none, .none, .none, .none, .none],
+    private var _p1Grid: [[Ships]] = [[.none, .none, .none, .none, .none, .none, .none, .none, .none, .none],
                                       [.none, .none, .none, .none, .none, .none, .none, .none, .none, .none],
                                       [.none, .none, .none, .none, .none, .none, .none, .none, .none, .none],
                                       [.none, .none, .none, .none, .none, .none, .none, .none, .none, .none],
@@ -40,7 +198,7 @@ class Game {
     
     // MY ships
     private var _p2Grid: [[Ships]] = [[.none, .none, .none, .none, .none, .none, .none, .none, .none, .none],
-                                      [.none, .none, .none, .none, .none, .ship, .none, .none, .none, .none],
+                                      [.none, .none, .none, .none, .none, .none, .none, .none, .none, .none],
                                       [.none, .none, .none, .none, .none, .none, .none, .none, .none, .none],
                                       [.none, .none, .none, .none, .none, .none, .none, .none, .none, .none],
                                       [.none, .none, .none, .none, .none, .none, .none, .none, .none, .none],
