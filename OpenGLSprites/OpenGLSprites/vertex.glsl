d1819a36bab75a93b14014e5d22b3e8c11ba679d
u0939404 <u0939404@1120-mac-07.lib.utah.edu>
2017-04-03 16:16:08 -0600

    Drawing moving triangles

--- OpenGLSprites/OpenGLSprites/vertex.glsl
+++ OpenGLSprites/OpenGLSprites/vertex.glsl
@@ -1,3 +1,8 @@
+
+attribute vec2 position;
+
+uniform vec2 translate;
+
 void main() {
-    gl_Position = vec4(0.0, 0.0, 0.0, 0.0)
+    gl_Position = vec4(position.x + translate.x, position.y + translate.y, 0.0, 1.0);
 }
